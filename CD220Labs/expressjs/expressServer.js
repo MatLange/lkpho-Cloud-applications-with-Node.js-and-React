ddc486ddbe8596175116136009514d44e3ca01ea
MatLange <matti.lange@gmx.de>
2024-03-23 17:26:25 +0100

    Update expressServer.js from Lab task Week3

--- CD220Labs/expressjs/expressServer.js
+++ CD220Labs/expressjs/expressServer.js
@@ -20,6 +20,17 @@ app.get("/:name",(req,res)=>{
     res.send("Hello "+req.params.name)
 })
 
+app.get("/fetchMonth/:num",(req,res)=>{
+    const month = ['January','February','March','April','May','June','July','August','September','October','November','December'];
+    
+    if (req.params.num < 1 || req.params.num > 12) {
+        res.send("Invalid month number ");
+        return;
+    }
+    const monthNumber = req.params.num - 1;
+    res.send("Month: " + month[monthNumber]);
+})
+
 app.listen(3333, () => {
     console.log(`Listening at http://localhost:3333`)
 })
